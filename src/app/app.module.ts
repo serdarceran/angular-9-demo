839b7229c7e34709e507b663e6c7a6b440caa668
serdarceran <ceranbs@gmail.com>
2020-11-15 23:39:14 +0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { FocusNextDirective } from "./focus-next.directive";
 import { ApplyPanelComponent } from "./apply-panel/apply-panel.component";
+import { ChipDemoComponent } from "./chip-demo/chip-demo.component";
 import { ChipsModule } from "primeng/chips";
-import { ChipDemoComponent } from './chip-demo/chip-demo.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ChipsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ChipsModule,
+    BrowserAnimationsModule,
+    ChipsModule,
+    ButtonModule
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
