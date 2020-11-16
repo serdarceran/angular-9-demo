440e300929eca569fb45e00b685d68fed570d547
serdarceran <ceranbs@gmail.com>
2020-11-16 19:16:38 +0300

    1 file modified

--- src/app/chip-demo/number-format.directive.ts
+++ src/app/chip-demo/number-format.directive.ts
@@ -1,21 +1,35 @@
-import { Directive, EventEmitter,  HostListener, Input, OnInit, Output } from "@angular/core";
+import {
+  ContentChild,
+  Directive,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnInit,
+  Output
+} from "@angular/core";
+import { Chips } from "primeng/chips";
 import { Observable } from "rxjs";
 
 @Directive({
   selector: "[appNumberFormat]"
 })
 export class NumberFormatDirective implements OnInit {
+  @ContentChild(Chips) myChips: Chips;
   @Output()
   numberAdded = new EventEmitter<number>();
 
   constructor() {}
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 
   @HostListener("onAdd", ["$event"])
-  clickEvent(event: {value:string}) {
-    const vv = +event.value.replace(',', '.');
-    this.numberAdded.emit(vv);
+  clickEvent(event: { value: string }) {
+    const vv = +event.value.replace(",", ".");
+    if (isFinite(vv)) {
+      console.log(`emit`, vv);
+      this.numberAdded.emit(vv);
+    } else {
+      this.myChips.value.pop();
+      console.error(`wrong format`);
+    }
   }
 }
